2c8a78a363a2cb9fde3911338b479d8eeea38b1e
xX-Higgs-Xx <133397981+xX-Higgs-Xx@users.noreply.github.com>
2025-01-12 19:04:54 -0600

    actualizacion de colores, responsividad en paginas e implementacion de carrucel automatizado en pantalla de cinema

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				tittle: ['Dynalight', 'serif'],
-				montserrat: ['Montserrat', 'sans-serif'],
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                principal: '#24272A',
+                secundario: '#343A40',
+            },
+            fontFamily: {
+                tittle: ['Dynalight', 'serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
 }
